71c01e71e33a40801225826ae76455c39f261814
Maurice Maier <maurice-maier@outlook.de>
2017-07-13 20:46:04 +0200

    Changed framework version to 4.6.2 and added CSharpScriptCommandPlugin

---
+++ CSharpScriptCommandsPlugin/CSharpScriptCommand.cs
@@ -0,0 +1,19 @@
+﻿using CommandPalette.PluginSystem;
+
+namespace CSharpScriptCommandsPlugin
+{
+    public class CSharpScriptCommand : ICommand
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool RunInUIThread => false;
+
+        public string Code { get; set; }
+
+        public void Execute()
+        {
+        }
+    }
+}
