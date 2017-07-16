38009211f5ac6c35342925fdba037efc4db0038c
DarkIrata <irata@irataprojects.de>
2017-07-16 16:19:52 +0200

    Handled possible Exceptions in CSharpScriptCommand and made it execute faster

--- CSharpScriptCommandsPlugin/CSharpScriptCommand.cs
+++ CSharpScriptCommandsPlugin/CSharpScriptCommand.cs
@@ -1,5 +1,11 @@
-﻿using CommandPalette.PluginSystem;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using CommandPalette.PluginSystem;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 
 namespace CSharpScriptCommandsPlugin
 {
@@ -15,11 +21,45 @@ namespace CSharpScriptCommandsPlugin
 
         public void Execute()
         {
-            var result = CSharpScript.EvaluateAsync<string>(this.Code).Result;
-            if (!string.IsNullOrEmpty(result))
+            if (string.IsNullOrEmpty(this.Code))
             {
-                System.Windows.MessageBox.Show(result);
+                MessageBox.Show(nameof(this.Code) + " can't be empty");
+                return;
             }
+
+            try
+            {
+                var result = CSharpScript.EvaluateAsync<string>(this.Code)?.Result;
+                if (!string.IsNullOrEmpty(result))
+                {
+                    MessageBox.Show(result);
+                }
+            }
+            catch (CompilationErrorException compilerError)
+            {
+                var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Log");
+                var logPath = Path.Combine(path, $"CompileError_{DateTime.Now.ToString("dd-MM-yy__HH_mm")}.log");
+
+                Directory.CreateDirectory(path);
+                File.WriteAllText(logPath, this.FormatCompileError(compilerError.Message));
+
+                MessageBox.Show($"Error while compiling the code.\r\nLOG: {logPath}", "WinCommand Palette", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private string FormatCompileError(string compileError)
+        {
+            var sb = new StringBuilder(2048);
+            sb.AppendLine("==============[ Info ]==============");
+            sb.AppendLine($"Command: '{this.Name ?? ">>InstantCommand-Call<<"}'");
+            sb.AppendLine(string.Empty);
+            sb.AppendLine("==============[ C# Code ]==============");
+            sb.AppendLine(this.Code);
+            sb.AppendLine(string.Empty);
+            sb.AppendLine("==============[ Compiler Error ]==============");
+            sb.AppendLine(compileError);
+
+            return sb.ToString();
         }
     }
 }
---
+++ CSharpScriptCommandsPlugin/CSharpScriptCommandsPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandPalette.PluginSystem;
+
+namespace CSharpScriptCommandsPlugin
+{
+    public class CSharpScriptCommandsPlugin : IInstantCommands
+    {
+        public List<ICommand> GetCommands()
+        {
+            return null;
+        }
+
+        public CSharpScriptCommandsPlugin()
+        {
+            Task.Run(() =>
+            {
+                // Running the compiler atleast one time will make him faster for further calls
+                var cmd = new CSharpScriptCommand()
+                {
+                    Code = "using System;"
+                };
+
+                cmd.Execute();
+            });
+        }
+    }
+}
