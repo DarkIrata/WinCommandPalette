64931e016f0e8875dc61969f4232c4f912fdd632
DarkIrata <irata@irataprojects.de>
2017-07-16 22:42:49 +0200

    Fixed inverted RunInUIThread Icon display

---
+++ WinCommandPalette/Converter/InvertBoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WinCommandPalette.Converter
+{
+    public class InvertBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (bool.TryParse(value.ToString(), out bool boolValue))
+            {
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
